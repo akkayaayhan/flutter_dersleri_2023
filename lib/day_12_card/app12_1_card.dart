4d63e993421e817d4a896e0bf6c0056afd0b30bb
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-04-15 01:42:05 +0300

    day12 bitti

---
+++ lib/day_12_card/app12_1_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Card Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Card Example'),
+        ),
+        body: Center(
+          child: Card(
+            elevation: 5.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Container(
+              width: 200,
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.favorite, size: 50, color: Colors.red),
+                  SizedBox(height: 10),
+                  Text(
+                    'I love Flutter!',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/day_12_card/card.md
@@ -0,0 +1,8 @@
+elevation: Card'ın yükseliği, gölgelendirme efekti ile belirlenir. Değer olarak bir double tipi alır.
+shape: Card'ın şekli, belirtilen BorderRadius değerine göre belirlenir. Değer olarak bir RoundedRectangleBorder tipi alır.
+child: Card'ın içinde gösterilecek widget'ı belirler. Değer olarak bir Widget tipi alır.
+Container widget'ı, Card widget'ının içinde yer alan bir child widget'ıdır ve ayrıca boyutunu ve hizalamasını ayarlamak için kullanılır.
+
+Column widget'ı, dikey bir dizi halinde birden fazla widget'ı tutmak için kullanılır. Bu örnekte, Column widget'ı, bir Icon widget'ı ve bir Text widget'ı içerir. Icon widget'ı, kalp simgesi olan kırmızı bir daire gösterirken, Text widget'ı, "I love Flutter!" metnini gösterir. Ayrıca, SizedBox widget'ı kullanarak iki widget arasında boşluk eklenmiştir.
+
+Sonuç olarak, yukarıdaki örnekteki Card widget'ı, yuvarlatılmış kenarlara, yükseltilmiş bir görünüme ve içinde dikey olarak hizalanan bir simge ve metin içeren bir içerik widget'ına sahiptir.
